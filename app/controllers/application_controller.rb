ec7c35435c19947def28b69e2a91d4f4d0fae54a
Quintin Adam <quintinadam@live.com>
2015-02-28 10:57:19 -0700

    guard file and console when dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_controller, :if => :is_development
+
+  private
+
+  def is_development
+    Rails.env.development?
+  end
+
+  def set_controller
+    console
+  end
+
 end
