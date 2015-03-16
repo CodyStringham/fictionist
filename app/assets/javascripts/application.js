ee7dc1b67cb3e9b0aee8264bda27401b1e4cf3c4
codystringham <cs.negativ@gmail.com>
2015-03-15 20:38:13 -0600

    refines navbar and user display

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,8 @@
 //= require foundation/foundation
 //= require foundation/foundation.topbar
 //= require foundation/foundation.reveal
+//= require foundation/foundation.alert
+//= require foundation/foundation.dropdown
 //= require masonry/jquery.masonry
 //= require masonry/jquery.event-drag
 //= require masonry/jquery.imagesloaded.min
