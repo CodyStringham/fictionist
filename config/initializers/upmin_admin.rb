737206d3cc7a218f9eccda50fe95a4cd03cdae5d
Quintin Adam <quintinadam@live.com>
2015-03-21 13:29:20 -0600

    upmin basic config

---
+++ config/initializers/upmin_admin.rb
@@ -0,0 +1,3 @@
+Upmin.configure do |config|
+  config.models = [:content, :user]
+end
--- test/factories/contents.rb
+++ test/factories/contents.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :content do
     content_type "MyString"
-message "MyString"
-location "MyString"
-user nil
-published_at "2015-03-07 15:11:21"
-view_permission 1
+    message "MyString"
+    location "MyString"
+    user nil
+    published_at "2015-03-07 15:11:21"
+    view_permission 1
   end
 
 end
