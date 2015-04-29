f777eb205b34da0502eec13c37aacda9370c15e7
Quintin Adam <quintinadam@live.com>
2015-04-28 19:09:20 -0600

    fixes xray

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,19 +1,6 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require xray
 //= require foundation/foundation
 //= require foundation/foundation.topbar
 //= require foundation/foundation.reveal
@@ -25,7 +12,6 @@
 //= require masonry/jquery.infinitescroll.min
 //= require masonry/modernizr-transitions
 //= require isotope/jquery.isotope
-
 //= require turbolinks
 //= require_tree .
 
