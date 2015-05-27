395a0e0cc8773c4e41e2db65b5c25cd3d0c7e704
codystringham <cs.negativ@gmail.com>
2015-05-27 17:45:07 -0600

    mandrill setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.mandrillapp.com",
     port: 587,
     domain: ENV['DOMAIN_NAME'],
     authentication: "plain",
