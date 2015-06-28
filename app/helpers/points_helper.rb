8171706d7973733b78be5332fecae9ebfc0186cd
codystringham <cs.negativ@gmail.com>
2015-06-27 22:16:10 -0600

    lock symbol more noticable, adds links for social tasks, updates social text on earn points page

--- app/helpers/points_helper.rb
+++ app/helpers/points_helper.rb
@@ -1,5 +1,22 @@
 module PointsHelper
 
+  def social_text(effort)
+    case effort.kind
+    when "facebook_like" then "Facebook Like"
+    when "facebook_post" then "Facebook Post"
+
+    when "twitter_like" then "Twitter Follow"
+    when "twitter_post" then "Twitter Tweet"
+
+    when "instagram_like" then "Instagram Follow"
+    when "instagram_post" then "Instagram Post"
+
+    when "youtube_like" then "Youtube Subscribe"
+    when "youtube_post" then "Youtube Video"
+    end
+  end
+
+
   def get_effort(name)
     Effort.find_by(name: name.split("_").join(" ").titleize).try(:id)
   end
